287c4900de933da20b9b34be0537c91d56f215bb
Sohanpy <sohanmock@gmail.com>
2019-09-21 16:40:17 +0600

    navbar added

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import nav from "./nav";
+import SearchBox from "./SearchBox";
+
+function App() {
+  return (
+    <div>
+      <nav />
+      <SearchBox />
+    </div>
+  );
+}
+
+export default App;
