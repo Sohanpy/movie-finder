e0b230bbda4b3173ec571a49db7ffa313fc714dd
Sohanpy <sohanmock@gmail.com>
2019-09-22 17:59:48 +0600

    Movie List component added

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import nav from "./nav";
 import SearchBox from "./SearchBox";
 
-function App() {
-  return (
-    <div>
-      <nav />
-      <SearchBox />
-    </div>
-  );
-}
+import MovieList from "./MovieList";
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: [],
+      searchItem: ""
+    };
+    this.apiKey = "a63db4d8ed9a5af15e2dd254142cdf3f";
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=${this.apiKey}&query=${this.state.searchItem}`
+    )
+      .then(data => data.json())
+      .then(data => {
+        console.log(data);
+        this.setState({ movies: [...data.results] });
+      });
+  };
+
+  handleChange = e => {
+    this.setState({ searchItem: e.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <nav />
+        <SearchBox
+          handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+        />
+        <MovieList movies={this.state.movies} />
+      </div>
+    );
+  }
+}
 export default App;
